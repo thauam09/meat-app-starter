1f33a2dbe2ff6967168ba868ee534b1315dfa70b
C1323554 Thaua Silva Martins <C1323554@interno.bb.com.br>
2021-11-06 00:40:48 -0300

    criacao do componente de reviews

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Observable } from 'rxjs';
+import { Review } from './review.model';
 
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html',
 })
 export class ReviewsComponent implements OnInit {
+  reviews: Observable<Review[]>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
+    const id = this.route.parent.snapshot.params['id'];
+    
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(id);
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Http } from "@angular/http";
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { ErrorHandler } from "app/app.error-handler";
+import { Review } from "app/restaurant-detail/reviews/review.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -20,6 +21,12 @@ export class RestaurantsService {
 
   restaurantById(id: string): Observable<Restaurant> {
     return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<Review[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
