e8f32418763459044cba4ad4f8c5e60426744f4d
thauam09 <thauam_09martins@hotmail.com>
2022-01-22 16:06:40 -0300

    criacao de um core module para servicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,16 @@ import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
-import { RestaurantsService } from "./restaurants/restaurants.service";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
-import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
 import { FormsModule } from "@angular/forms";
-import { OrderService } from "./order/order.service";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -41,16 +39,10 @@ import { SharedModule } from "./shared/shared.module";
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule,
-    ReactiveFormsModule,
     SharedModule,
+    CoreModule,
   ],
-  providers: [
-    RestaurantsService,
-    ShoppingCartService,
-    OrderService,
-    { provide: LOCALE_ID, useValue: "pt-BR" },
-  ],
+  providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
