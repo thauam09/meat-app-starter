23c4a696b2438513aa4a36a7d367643e3e627358
thauam09 <thauam_09martins@hotmail.com>
2022-02-16 00:00:18 -0300

    utilizando operadores do e switchMap para corrigir timer snackbar

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -9,6 +9,8 @@ import {
 import { NotificationService } from "../notification.service";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/observable/timer";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/switchMap";
 
 @Component({
   selector: "mt-snackbar",
@@ -42,13 +44,13 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) {}
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe((message) => {
-      this.message = message;
-      this.snackVisibility = "visible";
-      Observable.timer(3000).subscribe(() => {
-        this.snackVisibility = "hidden";
-      });
-    });
+    this.notificationService.notifier
+      .do((message) => {
+        this.message = message;
+        this.snackVisibility = "visible";
+      })
+      .switchMap((message) => Observable.timer(3000))
+      .subscribe((timer) => (this.snackVisibility = "hidden"));
   }
 
   toggleSnack() {
