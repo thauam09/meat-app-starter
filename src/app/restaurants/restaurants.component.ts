c2d8a49ad384ba8f533062de217adfd3ca3ee8cf
thauam09 <thauam_09martins@hotmail.com>
2022-04-01 00:12:27 -0300

    criacao da animacao da search bar

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,19 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { Restaurant } from './restaurant/restaurant.model';
-import { RestaurantsService } from './restaurants.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Restaurant } from "./restaurant/restaurant.model";
+import { RestaurantsService } from "./restaurants.service";
 
 @Component({
-  selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html',
+  selector: "mt-restaurants",
+  templateUrl: "./restaurants.component.html",
+  animations: [
+    trigger("toggleSearch", [
+      state(
+        "hidden",
+        style({
+          opacity: 0,
+          "max-height": "0px",
+        })
+      ),
+      state(
+        "visible",
+        style({
+          opacity: 1,
+          "max-height": "70px",
+          "margin-top": "20px",
+        })
+      ),
+      transition("* => *", animate("250ms 0s ease-in-out")),
+    ]),
+  ],
 })
 export class RestaurantsComponent implements OnInit {
   restaurants: Restaurant[];
 
-  constructor(private restaurantsService: RestaurantsService) { }
+  searchBarState = "hidden";
+
+  constructor(private restaurantsService: RestaurantsService) {}
 
   ngOnInit() {
-    this.restaurantsService.restaurants()
-      .subscribe(restaurants => this.restaurants = restaurants);
+    this.restaurantsService
+      .restaurants()
+      .subscribe((restaurants) => (this.restaurants = restaurants));
   }
 
+  toggleSearchBar(): void {
+    this.searchBarState =
+      this.searchBarState === "hidden" ? "visible" : "hidden";
+  }
 }
